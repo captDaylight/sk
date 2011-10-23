7ba4441213fd684b46641aeb37961ad57a25d34d
paul christophe <paul.christophe6@gmail.com>
2011-10-23 14:58:49 -0400

    up to heroku

--- sk/settings-local.py
+++ sk/settings-local.py
@@ -85,6 +85,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+	'django.contrib.comments',
+    'ajaxcomments',    
     'sooouk',
     'auth',
 )
