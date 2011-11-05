4fb7a216f22b070ae237e80e1270b862f499a7c7
paul christophe <paul.christophe6@gmail.com>
2011-11-05 13:17:56 -0400

    css

--- sk/media/js/index.js
+++ sk/media/js/index.js
@@ -2,26 +2,35 @@ $(document).ready(function() {
 	var filterClicked = 0; // starts at zero, but filter one is 1, filter two is 2 ...
 	var currentFilter = 0; // keep track of which filter is open.  0 == none selected
 	var filters = ['countries', 'categories', 'people'];
-
-	$('a').click(function(){
+	var subfilters = ['countriesfilter','categoriesfilter','peoplefilter']
+	
+	$('li').click(function(){
 		if($(this).attr('id') == filters[0]){
-			if(currentFilter == 1){
-				closeSubFilter();
-			}else{
-				
-			}
-			openClose();
+			filterClicked = 1;
+			openCloseFilter();
 		}else if($(this).attr('id') == filters[1]){
-			openClose()
+			filterClicked = 2;
+			openCloseFilter();
 		}else if($(this).attr('id') == filters[2]){
-			openClose();
+			filterClicked = 3;
+			openCloseFilter();
 		}
 	});
 	
-	function openSubFilter(){
-	
-	}
-	function closeSubFilter(){
-	
+	function openCloseFilter(){
+		if(filterClicked == currentFilter){
+			$('#'+subfilters[filterClicked-1]).hide(100);
+			currentFilter = 0;
+		}else{
+			if(currentFilter > 0){
+				$('#'+subfilters[currentFilter-1]).hide();
+				$('#'+subfilters[filterClicked-1]).show();
+				currentFilter = filterClicked;
+			}
+			else{
+				$('#'+subfilters[filterClicked-1]).show(100);
+				currentFilter = filterClicked;
+			}
+		}
 	}
 });
